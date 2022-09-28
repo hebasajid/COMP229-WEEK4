3fcee60b3840c8bda541db655992f7b5e1f2cf3f
Heba Sajid <hsajid35467@gmail.com>
2022-09-28 17:31:39 -0400

    initial commit

---
+++ public/scripts/app.js
@@ -0,0 +1,13 @@
+/*
+Name: Heba Sajid
+ID: 301244428
+Date: September 27th 2022
+*/
+(function(){
+
+    function Start()
+    {
+        console.log("App started!");
+    }
+    window.addEventListener('load', Start);
+}) ();
