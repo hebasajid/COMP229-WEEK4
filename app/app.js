558a6e73849b03e20fd060b0fd7cc33a8a43fe06
Heba Sajid <hsajid35467@gmail.com>
2022-10-17 22:16:08 -0400

    api authorization added

--- app/app.js
+++ app/app.js
@@ -9,21 +9,23 @@ import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-//autho step 1 : import modules
-
+// Auth Step 1 - import modules
 import passport from 'passport';
 import passportLocal from 'passport-local';
 import flash from 'connect-flash';
 
-//step 2: define our authentication stragety
+// modules for JWT Support
+import cors from 'cors';
+import passportJWT from 'passport-jwt';
+
+let JWTStrategy = passportJWT.Strategy;
+let ExtractJWT = passportJWT.ExtractJwt;
+
+// Auth Step 2 - defien our auth strategy
 let localStrategy = passportLocal.Strategy;
 
-//step 3 - import user model
+// Auth Step 3 - import the user model
 import User from './models/user.js';
-
-//step 4 - 
-
-
 
 // Import Mongoose Module
 import mongoose from 'mongoose';
@@ -34,9 +36,10 @@ import { MongoURI, Secret } from '../config/config.js';
 // Import Routes
 import indexRouter from './routes/index.route.server.js'
 import movieRouter from './routes/movies.route.server.js';
-import contactRouter from './routes/contacts.route.server.js';
 import authRouter from './routes/auth.route.server.js';
 
+// Import API Routes
+import authApiRouter from './routes/api/auth-api.route.server.js';
 
 // Instantiate Express Application
 const app = express();
@@ -62,34 +65,53 @@ app.use(cookieParser());
 // app.use(express.static(path.join(__dirname,'/client')));
 app.use(express.static(path.join(__dirname,'../public')));
 
-//AUTH step 4 - setup express session
+app.use(cors()); // adds CORS (cross-origin resource sharing) - To be removed on PRODUCTION
+
+// Auth Step 4 - Setup Express Session
 app.use(session({
     secret: Secret,
     saveUninitialized: false, 
     resave: false
 }));
 
-//auth step 5: setup flash:
+// Auth Step 5 -  Setup Flash
 app.use(flash());
 
-//AUTH step 6: initalize pasport and session'
+// Auth Step 6 - Initialize Passport and Session
 app.use(passport.initialize());
 app.use(passport.session());
 
-//auth step 7: implementing the auth strategy:
+// Auth Step 7 - Implementing the Auth Strategy
 passport.use(User.createStrategy());
 
-//auth step 8 - setup serializtion and deserialization:
+// Auth Step 8 - Setup serialization and deserialization
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+// Setup JWT options
+let jwtOptions = {
+    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+    secretOrKey: Secret
+}
+
+// Setup JWT Strategy
+let strategy = new JWTStrategy(jwtOptions, (jwt_payload, done) => {
+    User.findById(jwt_payload.id)
+        .then(user => {
+            return done(null, user)
+        })
+        .catch(err => {
+            return done(err, false)
+        });
+});
+
+passport.use(strategy);
 
 // Use Routes
 app.use('/', indexRouter);
-app.use('/', contactRouter);
 app.use('/', movieRouter);
 app.use('/', authRouter);
-
+app.use('/api/auth', authApiRouter);
 
 
 export default app;
--- app/utils/index.js
+++ app/utils/index.js
@@ -1,14 +1,32 @@
-export function UserDisplayName(req){ //utils check if we have a user inside a reques,t we have display naem availabe and if we dont we return blank
+import jwt from 'jsonwebtoken';
+import { Secret } from '../../config/config.js';
+
+export function UserDisplayName(req){
     if(req.user){
         return req.user.displayName;
     }
     return '';
 }
 
-//middleware another one
-export function AuthGuard(req,res,next){  //intercept request, checks if user is logged in, it=f not logged in athen no access
+export function AuthGuard(req, res, next){
     if(!req.isAuthenticated()){
-        return res.redirect('/login');
+        return res.redirect('/login')
     }
     next();
+}
+
+export function GenerateToken(user){
+    const payload = {
+        id: user._id,
+        displayName: user.displayName,
+        username: user.username,
+        emailAddress: user.EmailAddress
+    }
+
+    const jwtOptions = {
+        expiresIn: 604800 // 1 week
+    }
+
+    return jwt.sign(payload, Secret, jwtOptions);
+
 }
